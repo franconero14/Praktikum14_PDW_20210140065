fb7c99400e2901ae25009ed322d0ea4f05dd13f1
franconero14 <nerobalor@gmail.com>
2023-06-22 11:04:10 +0700

    finish form 2

--- Praktikum14_PDW_20210140065/Form2.cs
+++ Praktikum14_PDW_20210140065/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,22 +13,106 @@ namespace Praktikum14_PDW_20210140065
 {
     public partial class Form2 : Form
     {
-        public Form2()
+        private string stringConnection = "data source = ZEPHYRUS-NEROBA\\FRANCONERO;" +
+            "database=Data_Mahasiswa;multipleActiveResultSets=true;user ID= sa; Password = 28112012";
+        private SqlConnection koneksi;
+        private string kstr;
+        public FormDataProdi()
         {
             InitializeComponent();
+            koneksi = new SqlConnection(kstr);
+            refreshForm();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Form2_Load(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void Data_Prodi_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'data_MahasiswaDataSet.Prodi' table. You can move, or remove it, as needed.
+            this.prodiTableAdapter.Fill(this.data_MahasiswaDataSet.Prodi);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void refreshForm()
+        {
+            nmp.Text = "";
+            nmp.Enabled = false;
+            btnSaved.Enabled = false;
+            btnClear.Enabled = false;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dataGridView();
+            btnOpen.Enabled = false;
+        }
+
+        private void dataGridView()
+        {
+            koneksi.Open();
+            string str = "select nama_prodi from dbo.Prodi";
+            SqlDataAdapter da = new SqlDataAdapter(str, koneksi);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+            koneksi.Close();
+        }
+
+        private void btnAdd_Click(Object sender, EventArgs e)
+        {
+            nmp.Enabled = true;
+            btnSaved.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            refreshForm();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string nmProdi = nmp.Text;
+            if (nmProdi == "")
+            {
+                MessageBox.Show("Masukkan Nama Prodi", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                koneksi.Open();
+                string str = "Insert into dbo.Prodi (nama_prodi)" + "values(@id)";
+                SqlCommand cmd = new SqlCommand(str, koneksi);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(new SqlParameter("id", nmProdi));
+                cmd.ExecuteNonQuery();
+
+                koneksi.Close();
+                MessageBox.Show("Data Berhasil Disimpan", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dataGridView();
+                refreshForm();
+            }
+        }
+
+        private void FormDataProdi_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FormHalamanUtama hu = new FormHalamanUtama();
+            hu.Show();
+            this.Hide();
+        }
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
